7933b8092326fd1a6829b92e34bcf5b0b1ffab1e
paulinatorresh <paulimela@icloud.com>
2025-02-25 22:09:37 +0100

    Updated the edit task function to be able to add/ edit a comment

---
+++ operators.py
@@ -0,0 +1,24 @@
+print("hello world")
+print("I can do this all day")
+print("another line of text")
+
+print(not True)
+print(not False)
+print(not 7) #False
+
+print(True or False)
+print(True and False)
+print(2 or 3 or 4)
+print(False or 0 or 4 or True)
+print(1 and 2 and 3)
+print(1 and 0.0 and 4)
+print(False or 0 or None)
+print(2+6)
+print(9-99)
+print(3*8)
+print(20/5) #  float result
+print(22/5)
+print(20//5) # integer division
+print(22%5)
+print(10%6)
+print(2**3)
